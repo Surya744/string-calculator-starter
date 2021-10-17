66a1742d16db21489b9849032c64f65ad15c3da9
Krishna <krishnasuryawanshi744@gmail.com>
2021-10-17 13:15:23 +0530

    test case 1.1

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,11 +3,23 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
+    	
+    	String[] no = input.split(",");
         
     	if(isEmpty(input)) {
     		return 0;
     	}
-		return stringToInt(input);
+    	if(input.length() == 1){
+    		
+    		return stringToInt(input);
+    		
+    	}
+    	else {
+    		
+    		return Integer.parseInt(no[0]) + Integer.parseInt(no[1]);
+    		
+    	}
+		
    
     }
 
